24828b4abba7eb49daa017e2bda775edb5369420
RadoslawSzczepanski <radoslaw.szczepanski327@gmail.com>
2025-04-02 14:21:34 +0000

    3_2_1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        int wysokosc = 3; 
+        int wysokosc = 5; 
+        int liczbaPowtorzen = 3; 
 
-        for (int i = 0; i < wysokosc; i++) {
-            for (int j = 0; j < i; j++) {
-                System.out.print(" ");
+        for (int k = 0; k < liczbaPowtorzen; k++) { 
+            for (int i = 0; i < wysokosc; i++) {
+                for (int j = 0; j < i; j++) {
+                    System.out.print(" ");
+                }
+                for (int j = 0; j < (2 * (wysokosc - i) - 1); j++) {
+                    System.out.print("*");
+                }
+                System.out.println(); 
             }
-            for (int j = 0; j < (2 * (wysokosc - i) - 1); j++) {
-                System.out.print("*");
-            }
-            System.out.println();
+            
         }
     }
 }
